d3165628a5d0106f53e7128d84889746365f0dd0
ieuanisrael <ieuan.israel@ludisanalytics.com>
2024-04-17 12:26:07 -0400

    Initial commit

---
+++ R/functions.R
@@ -0,0 +1,43 @@
+#' Add together two numbers
+#'
+#' @param url url corresponding to datasets s3 location
+#' @param headers headers containing a ludis api token
+#' @returns return a httr::PUT reponse object
+#' @export
+write_ludis_csv <- function(url, headers) {
+  csv_data <- capture.output(write.csv(data_frame, row.names = F))
+  
+  response <- httr::PUT(url = url,
+                        httr::add_headers(.headers=headers))
+  
+  response <- httr::PUT(url = response$url,body = csv_data)
+  
+  return(response)
+}
+
+#' Add together two numbers
+#'
+#' @param url url corresponding to datasets s3 location
+#' @param headers headers containing a ludis api token
+#' @returns a dataframe `df`
+#' @export
+read_ludis_csv <- function(url, headers) {
+  res <- httr::GET(url = url, httr::add_headers(.headers=headers))
+  
+  df <- readr::read_csv(rawToChar(res$content))
+  
+  return(df)
+}
+
+#' Add together two numbers
+#'
+#' @param url url corresponding to datasets s3 location
+#' @param headers headers containing a ludis api token
+#' @returns a dataframe `df`
+#' @export
+list_ludis_datasets <- function(url, headers) {
+  res <- httr::GET(url = url, httr::add_headers(.headers=headers))
+  df <- fromJSON(rawToChar(res$content))
+  
+  return(df$listS3FolderContents$Contents)
+}
