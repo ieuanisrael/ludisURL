6ed12d157fd08dd644acd3bb25e13feb12574706
ieuanisrael <ieuan.israel@ludisanalytics.com>
2024-04-17 12:53:45 -0400

    add data_frame param to ludis r package

--- R/functions.R
+++ R/functions.R
@@ -1,10 +1,11 @@
 #' Add together two numbers
 #'
 #' @param url url corresponding to datasets s3 location
-#' @param headers headers containing a ludis api tokenfdsfdsa
+#' @param headers headers containing a ludis api token
+#' @param data_frame the dataframe to be written to ludis dataset
 #' @returns return a httr::PUT reponse object
 #' @export
-write_ludis_csv <- function(url, headers) {
+write_ludis_csv <- function(data_frame, url, headers) {
   csv_data <- capture.output(write.csv(data_frame, row.names = F))
 
   response <- httr::PUT(url = url,
